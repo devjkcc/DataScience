a4fca34931687f677dec9118f88f130ad1475952
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-04 17:27:27 -0300

    EntradaDeDados

---
+++ PythonBasico/Input.py
@@ -0,0 +1,8 @@
+#Trabalhando com entrada de dados
+
+name =input("informe seu nome: ")
+print(name)
+# Python sempre retorna as variáveis como inteiros
+idade = int(input("Informe sua idade: "))
+print(idade) 
+print("Seu nome é: " ,name, " e sua idade é ", idade)
