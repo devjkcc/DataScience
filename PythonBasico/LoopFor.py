5b287605ccc89e9a3917268343dfe458f1d0d6e8
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-04 17:53:33 -0300

    LoopFor

---
+++ PythonBasico/LoopFor.py
@@ -0,0 +1,17 @@
+#Trabalhando com for
+
+for i in range(3):
+ print(i)
+
+for i in range(3):
+  print("Ola") 
+
+
+a=0
+b=5
+for a in range(b):
+ print(a)
+
+ curso = "Python"
+ for i in curso:
+  print(i)
