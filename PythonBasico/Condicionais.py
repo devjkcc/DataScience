2aa44388bf0450f580bdfa0aa092feb4d65f6c59
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-04 17:46:24 -0300

    Condicionais

---
+++ PythonBasico/Condicionais.py
@@ -0,0 +1,10 @@
+#Trabalhando com condicionais
+
+valor1 = int(input("Digite um valor: "))
+
+if valor1 > 2:
+  print("valor 1 é maior")
+elif valor1==2:
+  print("valor1 é igual")
+else:
+  print("valor 1 é menor")
