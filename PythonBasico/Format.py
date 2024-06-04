2a9018bc5f199b8aa806c073f9e84b6b09d5a6f8
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-04 17:38:38 -0300

    Format

---
+++ PythonBasico/Format.py
@@ -0,0 +1,11 @@
+sabor1 = "chocolate"
+sabor2 = "morango"
+print('Gosto de sorvete de {}'.format(sabor1))
+
+print("Gosto de sorvete de %s"%sabor1)
+
+print('Gosto de sorvete de {0}, voce gosta de {1}'.format(sabor1, sabor2))
+
+conta = 1/3
+#Formatando valores
+print('O resultado foi {:.2f}'.format(conta))
