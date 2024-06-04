de039959ec08f29c81740c0eb93568bd7f1e8f9f
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-04 17:06:41 -0300

    3commit

---
+++ PythonBasico/Operadores.py
@@ -0,0 +1,12 @@
+# Soma básica
+print(5+2)
+#Subtração básica
+print(5-2)
+#Divisão com resto
+print(5/2)
+#Divisão sem resto
+print(5//2)
+#Multiplicação simples
+print(5*2)
+#Exponenciação simples
+print(5**2)
