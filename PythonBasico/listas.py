541562fa0f00b89885effa3ad7e7c954d964d7b6
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-10 18:50:28 -0300

    listas

---
+++ PythonBasico/listas.py
@@ -0,0 +1,12 @@
+lista01 = [1,2,3]
+lista03 = [4,5,6]
+lista02 = [[1,2,3], [4,5,6], [7,8,9]]
+print(lista01)
+print(lista01[1])
+print(lista02[1])
+print(lista02[0][1])
+lista01.append(4)
+print(lista01)
+print(lista01 + lista03)
+lista01.sort()
+print(lista01)
