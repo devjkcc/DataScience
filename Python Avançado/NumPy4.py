69e383189ec459614588b8434c2e33207b423cd6
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-18 14:56:57 -0300

    NumPy4

---
+++ Python Avançado/NumPy4.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#Broadcasting 
+#Operações matemáticas com arrays
+data = np.array([1,2,3,4,5])
+print(data*1.6)
+print(data.sum())
+print(data.max())
+print(data.min())
+print(data.mean())
+print(data.prod())
+print(data.std()) # --> desvio padrão
