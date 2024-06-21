11d9d1394caec369704b2c451ae5262dc8c5428b
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-21 16:03:47 -0300

    MatPlot2

--- Python Avançado/MatPlot2.py
+++ Python Avançado/MatPlot2.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 valor = [2,4,6,8,10,12,14,16]
 produto=[1,2,3,4,5,6,7,8]
 # Usado para criar gráficos de dispersão
 plt.scatter(valor,produto)
+x = np.arange(0,100,1)
+plt.plot(x,x**2)
+print(plt.show()) # Gerando um gráfico exponencial
