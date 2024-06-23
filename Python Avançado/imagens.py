bb9e6f161f9215f2708a7a4c3e95c87d842c42c6
Jefferson <jeffersonkauancavalcante@gmail.com>
2024-06-23 12:25:32 -0300

    imagens

---
+++ Python Avançado/imagens.py
@@ -0,0 +1,6 @@
+from skimage.io import imread
+import matplotlib.pyplot as plt
+#Mostrando imagem
+imagem = imread('C:/Users/jeffe/OneDrive/Área de Trabalho/Documentos/Python2024/imagens/passaro.png')
+plt.imshow(imagem)
+plt.show()
